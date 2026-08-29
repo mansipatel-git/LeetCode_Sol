c798e4dfe77266ec37b19c1a134d7756b9c86146
Mansi Patel <158139515+mansipatel-git@users.noreply.github.com>
2026-08-29 10:38:49 +0530

    Time: 4 ms (10.64%), Space: 71.8 MB (52.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,11 +3,12 @@ public:
     int maxSubArray(vector<int>& nums) {
         int curr = nums[0];
         int ans = nums[0];
-        for(int i = 1; i < nums.size() ; i++){
-            curr = max(curr + nums[i] , nums[i]);
-            ans = max(ans ,curr);
-        }
 
+        for(int i = 1 ; i < nums.size() ; i++){
+            curr = max(nums[i] , curr + nums[i]);
+            ans = max(ans , curr);
+        }
         return ans;
+
     }
 };
